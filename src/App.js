0c88eed036020ff87fb83977fba5863dae7bc844
genival.junior <genivaljunior86@gmail.com>
2022-10-24 19:27:44 -0300

    Commit Inicial + Antd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { DatePicker } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DatePicker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
